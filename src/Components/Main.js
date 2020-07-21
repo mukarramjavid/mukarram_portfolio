ed7d564c91075363964b3e276e5dc33e830f439e
Mukarram Javid <mukarramjavid@gmail.com>
2020-07-21 00:13:51 -0700

    Update Main.js

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -19,9 +19,9 @@ const Main = () => {
           <Col md={9} xs={12}>
             <Switch>
               <Route path="/" exact component={Home} />
-              <Route  path="/skills" component={Skills} />
-              <Route  path="/services" component={Services} />
-              <Route  path="/contact" component={Contact} />
+              <Route  path="/skills" exact component={Skills} />
+              <Route  path="/services" exact component={Services} />
+              <Route  path="/contact" exact component={Contact} />
             </Switch>
           </Col>
         </Row>
