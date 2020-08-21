ee951e794f8e7bac261997c2d275fb8b23c82ac1
Mukarram Javid <mukarramjavid@gmail.com>
2020-08-21 08:21:45 +0500

    add data files

---
+++ src/ServiceData.js
@@ -0,0 +1,33 @@
+const ServiceData = [
+  {
+    id: "1",
+    skillName: "Web Development",
+    iconName: "i-color fas fa-code fa-3x",
+  },
+  {
+    id: "2",
+    skillName: "Web Design",
+    iconName: "i-color fas fa-palette fa-3x",
+  },
+  {
+    id: "3",
+    skillName: "WinForm Development",
+    iconName: "i-color fas fa-laptop-code fa-3x",
+  },
+  {
+    id: "4",
+    skillName: "WordPress",
+    iconName: "i-color fab fa-wordpress fa-3x",
+  },
+  {
+    id: "5",
+    skillName: "Databases",
+    iconName: "i-color fas fa-database fa-3x",
+  },
+  {
+    id: "6",
+    skillName: "Support",
+    iconName: "i-color fas fa-headset fa-3x",
+  },
+];
+export default ServiceData;
---
+++ src/SkillData.js
@@ -0,0 +1,51 @@
+const skillData = [
+  {
+    id: "1",
+    skillName: "HTML/CSS",
+    skillVariant: "success",
+    skillLabel: "100%",
+    skillNow: 100,
+    animated: "",
+  },
+  {
+    id: "2",
+    skillName: "ASP.NET MVC 5",
+    skillVariant: "primary",
+    skillLabel: "80%",
+    skillNow: 80,
+    animated: "animated",
+  },
+  {
+    id: "3",
+    skillName: "jQuery/JS",
+    skillVariant: "warning",
+    skillLabel: "75%",
+    skillNow: 75,
+    animated: "animated",
+  },
+  {
+    id: "4",
+    skillName: "MSSQL Server",
+    skillVariant: "danger",
+    skillLabel: "65%",
+    skillNow: 65,
+    animated: "",
+  },
+  {
+    id: "5",
+    skillName: "Web API",
+    skillVariant: "info",
+    skillLabel: "15%",
+    skillNow: 15,
+    animated: "animated",
+  },
+  {
+    id: "6",
+    skillName: "React.js",
+    skillVariant: "warning",
+    skillLabel: "35%",
+    skillNow: 35,
+    animated: "",
+  },
+];
+export default skillData;
