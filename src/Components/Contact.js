762177e56a9b5b38bed572d563c411325f9f4cd7
Mukarram Javid <mukarramjavid@gmail.com>
2020-12-28 15:40:59 +0500

    remove fb-pg

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,20 +10,17 @@ export default function Contact() {
           </Col>
         </Row>
         <Row className="text-center Contact">
-          <Col xs={12} md={3} sm={3}>
+          <Col xs={12} md={4} sm={4}>
             <i class="fa fa-map-marker i-color fa-3x" aria-hidden="true"></i>
             <h4>Lahore, Pakistan</h4>
           </Col>
-          <Col xs={12} md={3} sm={3}>
+          <Col xs={12} md={4} sm={4}>
             <i class="fa fa-phone i-color fa-3x" aria-hidden="true"></i>
             <h4>+92 323 8883647</h4>
           </Col>
-          <Col xs={12} md={3} sm={3}>
+          <Col xs={12} md={4} sm={4}>
             <i class="fas fa-envelope i-color fa-3x" aria-hidden="true"></i>
             <h4>mukarramjavid@gmail.com</h4>
-          </Col>
-              <Col xs={12} md={3} sm={3}>
-            <div class="fb-page" data-href="https://www.facebook.com/UniversityWaliAwam" data-tabs="timeline" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/UniversityWaliAwam" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/UniversityWaliAwam">University Wali Awam</a></blockquote></div>
           </Col>
         </Row>
       </Container>
