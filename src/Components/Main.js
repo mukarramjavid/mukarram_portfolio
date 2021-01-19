b6c43b05e945255e29aca0a9736e0ac8e648d622
Mukarram Javid <mukarramjavid@gmail.com>
2021-01-19 22:01:54 +0500

    Add suspense

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import Sidebar from "./Sidebar";
-import Home from "./Home";
+/*import Home from "./Home";
 import Skills from "./Skills";
 import Services from "./Services";
 import Contact from "./Contact";
-import Footer from "./Footer";
+import Footer from "./Footer";*/
 import { Container, Row, Col } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 
+const Home = lazy(() => import('./Components/Home'));
+const About = lazy(() => import('./Components/About'));
+const Skills = lazy(() => import('./Components/Skills'));
+const Contact = lazy(() => import('./Components/Contact'));
 const Main = () => {
   return (
 <>
@@ -17,12 +21,14 @@ const Main = () => {
             <Sidebar />
           </Col>
           <Col md={9} xs={12}>
+          <Suspense fallback={<div>Loading ...</div>}>
             <Switch>
               <Route exact path="/"  component={Home} />
               <Route exact path="/skill"  component={Skills} />
               <Route exact path="/service"  component={Services} />
               <Route exact path="/contact"  component={Contact} />
             </Switch>
+          </Suspense>
           </Col>
         </Row>
       </Container>
